31b9e4487f696f5bbac325e53d927f510d95ac73
lucasew <lucas59356@gmail.com>
2025-11-10 16:05:57 -0300

    checkpoint
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- cmake/FindCMath.cmake
+++ cmake/FindCMath.cmake
@@ -1,14 +1,9 @@
-include(CheckLibraryExists)
-check_library_exists(m ceil "" LIBM)
-if(LIBM)
-    set(CMATH_LIBRARIES m)
-else()
-    set(CMATH_LIBRARIES)
-endif()
+find_library(CMATH_LIBRARY NAMES m)
 
-if(NOT TARGET CMath::CMath)
+if (CMATH_LIBRARY)
     add_library(CMath::CMath INTERFACE IMPORTED)
     set_target_properties(CMath::CMath PROPERTIES
-        INTERFACE_LINK_LIBRARIES "${CMATH_LIBRARIES}"
+        INTERFACE_LINK_LIBRARIES "${CMATH_LIBRARY}"
     )
+    set(CMATH_FOUND TRUE)
 endif()
